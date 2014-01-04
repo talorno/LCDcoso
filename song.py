2312146a8e40ede07c55adfd551636a4446b1765
Giovanni 'Talorno' Ricciardi <giovanni.talorno@gmail.com>
2014-01-04 01:41:39 +0100

    added song.py, some minor mods to coso.py

---
+++ song.py
@@ -0,0 +1,8 @@
+class song:
+
+    def __init__(self, title, artist, album, tracknumber, year):
+        self.title = title
+        self.artist = artist
+        self.album = album
+        self.tracknumber = tracknumber
+        self.year = year
